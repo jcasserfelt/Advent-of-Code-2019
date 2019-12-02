61caf03b55bf2b70238b820aec218d5ad024dea9
Johan Casserfelt <johan.casserfelt@gmail.com>
2019-12-02 01:06:42 +0100

    Day1

---
+++ day1/src/Main.java
@@ -0,0 +1,49 @@
+import javax.imageio.stream.ImageInputStream;
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        File input = new File("day1/input/input.txt");
+        List<Integer> inputList = new ArrayList<>();
+        List<Integer> resultList = new ArrayList<>();
+        List<Integer> testList = new ArrayList<>();
+        testList.add(100756);
+
+
+        BufferedReader reader = new BufferedReader(new FileReader(input));
+
+
+        while (true) {
+
+            String temp;
+            temp = reader.readLine();
+            if (temp == null) break;
+            else inputList.add(Integer.parseInt(temp));
+
+        }
+        System.out.println(inputList);
+
+        int resultSum = 0;
+        int modulemadness = 0;
+        for (Integer i : inputList) {
+            int temp = i;
+            modulemadness = 0;
+            while (temp > 0) {
+                temp = Math.floorDiv(temp, 3);
+                temp = temp - 2;
+                if(temp < 0) break;
+                modulemadness += temp;
+            }
+            resultList.add(modulemadness);
+            resultSum += modulemadness;
+        }
+        System.out.println(resultSum);
+
+    }
+}
+
